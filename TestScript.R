355e377b1a2d50c0a2b5267e45304dbe5b0b631e
wizlon72 <wbw4am@gmail.com>
2017-05-03 21:57:14 -0400

    initial download

---
+++ TestScript.R
@@ -0,0 +1,6 @@
+getwd()
+Washdef2015 <- read.csv("Beisbol/2015_Defense/Washington_Defense_2015.csv", header = TRUE)
+Washdef2016 <- read.csv("Beisbol/2016_Defense/Washington_Defense_2016.csv", header = TRUE)
+Arizdef2015 <- read.csv("Beisbol/2015_Defense/Arizona_Defense_2015.csv", header = TRUE)
+Arizdef2016 <- read.csv("Beisbol/2016_Defense/Arizona_Defense_2016.csv", header = TRUE)
+def2015
